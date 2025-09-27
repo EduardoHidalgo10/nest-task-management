8e7a6706227c0dedfe5464be7974d95ba6d35c65
Eduardo Hidalgo <eahidalgo@segurosreservas.com>
2025-09-27 08:48:05 -0400

    auth module & sign up method added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -16,7 +17,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database:'task-management',
       autoLoadEntities:true,
       synchronize:true,
-    })
+    }),
+    AuthModule
     
   ],
 })
